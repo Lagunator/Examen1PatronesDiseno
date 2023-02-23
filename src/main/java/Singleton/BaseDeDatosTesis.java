607e9571a43d88878ee13aa9cfa1c941460fd6de
Jhosias Laguna <jhosiasmauricio@gmail.com>
2023-02-23 19:03:35 -0400

    commit #1

---
+++ src/main/java/Singleton/BaseDeDatosTesis.java
@@ -0,0 +1,34 @@
+package Singleton;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class BaseDeDatosTesis {
+    private static BaseDeDatosTesis instance;
+    private Map<Tesis, Date> registro = new ConcurrentHashMap<>();
+
+    private BaseDeDatosTesis (){
+        System.out.println("Estamos generando la base de datos");
+    }
+    public static BaseDeDatosTesis getInstance(){
+        if(instance == null){
+            //multithreadControl();
+        }
+        return instance;
+    }
+    private synchronized void  registrarTesis(Tesis tesis, Date date) {
+        if (registro.containsKey(tesis)) {
+            System.out.println("Estamos registrando la tesis de:" + tesis.getDatosEstudiante().getNombre() + "en la fecha:" + date);
+            registro.put(tesis, date);
+        } else {
+            System.out.println("Error se encontro tesis duplicada");
+        }
+    }
+    public synchronized void verRegistro(){
+        System.out.println("Registro");
+        for (Map.Entry<Tesis,Date> entry: registro.entrySet()){
+            System.out.println(entry.getKey().getDatosEstudiante().getNombre()+"---->"+entry.getKey().getDatosEstudiante().getCi()+"----->"+entry.getKey().getTitulo()+"------>"+ entry.getKey().getMencion()+"----->"+ entry.getValue());
+        }
+    }
+}
---
+++ src/main/java/Singleton/JefeRecursosHumanos.java
@@ -0,0 +1,26 @@
+package Singleton;
+
+import java.util.Date;
+
+public class JefeRecursosHumanos {
+    private String nombre;
+    private String ci;
+
+    public JefeRecursosHumanos(){
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public String getCi(){
+        return ci;
+    }
+    public void setCi(String ci){
+            this.ci = ci;
+    }
+    public void registarTesis(Tesis tesis, Date date){
+        BaseDeDatosTesis.getInstance().verRegistro();
+    }
+}
